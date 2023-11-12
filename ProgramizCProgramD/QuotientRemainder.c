38168439c40a1060d50ff2268ce27d0bf3ce6999
MayankDDewali <mayankddewali@gmail.com>
2023-11-12 14:14:37 +0530

    improving this question

--- ProgramizCProgramD/QuotientRemainder.c
+++ ProgramizCProgramD/QuotientRemainder.c
@@ -1,5 +1,30 @@
-/*C Program to Compute Quotient and Remainder*/
-#include<stdio.h>
-int main(){
-    int 
+#include <stdio.h>
+
+int main() {
+    float dividend, divisor, quotient, remainder;
+
+    // Get user input
+    printf("Enter dividend: ");
+    scanf("%f", &dividend);
+
+    printf("Enter divisor: ");
+    scanf("%f", &divisor);
+
+    // Check if the divisor is not zero
+    if (divisor != 0) {
+        // Computes quotient
+        quotient = dividend / divisor;
+
+        // Computes remainder without fmod
+        remainder = dividend - quotient * divisor;
+
+        // Display results
+        printf("Quotient = %f\n", quotient);
+        printf("Remainder = %f\n", remainder);
+    } else {
+        // Handle division by zero
+        printf("Error: Division by zero is undefined.\n");
+    }
+
+    return 0;
 }
