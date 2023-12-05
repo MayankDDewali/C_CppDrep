a583cd14d7acf17058b09dd14b0ffff991d5b3ee
MayankDDewali <mayankddewali@gmail.com>
2023-12-05 12:56:28 +0530

    I pattern

---
+++ Name in pattern/I.c
@@ -0,0 +1,29 @@
+/*
+                  * 
+                  * 
+                  *
+                  * 
+                  * 
+*/
+
+#include <stdio.h>
+int main()
+{
+    int n, i, j;
+    n = 5;
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= 5; j++)
+        {
+            if(j==3)
+            {
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
