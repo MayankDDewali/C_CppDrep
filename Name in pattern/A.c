01b094c7c65d141cd6e8bfb03795ed19cb4fe470
MayankDDewali <mayankddewali@gmail.com>
2023-12-05 11:36:10 +0530

    A

---
+++ Name in pattern/A.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int row, i, j, k;
+    row = 5;
+    for (i = 1; i <= row; i++)
+    {
+        for (j = i; j < row; j++)
+        {
+            printf(" ");
+        }
+        for (k = 1; k <= (i * 2 - 1); k++)
+        {
+            if(k==1 || k==(2*i-1))
+            {
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
