b48d79002ed74a0b557475a6e816273b9475ab2f
MayankDDewali <149759601+MayankDDewali@users.noreply.github.com>
2024-02-07 22:46:52 +0530

    Update Pz9SwapTwoNumber.c

--- ProgramizCProgramD/Pz9SwapTwoNumber.c
+++ ProgramizCProgramD/Pz9SwapTwoNumber.c
@@ -1,1 +1,20 @@
+/* Pz9- C Program to Swap Two Numbers*/
 
+#include <stdio.h>
+int main()
+{
+    float a, b;
+    printf("Enter the first number: ");
+    scanf("%f", &a);
+    printf("Enter the second number: ");
+    scanf("%f", &b);
+    printf("\n\nBefore swapping the numbers...");
+    printf("\na = %.4f", a);
+    printf("\nb = %.4f", b);
+    printf("\n\nAfter swapping the numbers...");
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("\na = %.4f", a);
+    printf("\nb = %.4f", b);
+}
